92c58faefcffc4c2c113cffab980c4db07401531
Deepak Charan L <deepakcharan0108@gmail.com>
2023-02-04 17:49:50 -0600

    Corrected handin script

--- p2/handin.sh
+++ p2/handin.sh
@@ -10,21 +10,15 @@ projectNumber="${currentDirectory: -1}"
 handinDirectory="/home/cs537-1/handin/logavaseekaran/P$projectNumber/"
 projectFiles="$@"
 
-
-#echo "Contents of project directory P"$1": "
-#ls ~cs537-1/handin/logavaseekaran/P$1
-
 echo "Project: P$projectNumber"
 echo "Files to copy: $projectFiles"
 echo "Current Directory: $currentDirectory"
 echo "Handin Directory: $handinDirectory"
 echo "Copying files ..."
-# cp $projectFiles $handinDirectory
-filesCopied="$(ls $handinDirectory)"
-echo $filesCopied > fileCopied.txt
-echo $projectFiles > projectFiles.txt
-if [[ "$filesCopied" = "$projectFiles" ]]; then
+if cp $projectFiles $handinDirectory; then
     echo "Successfully Copied files to $handinDirectory"
+    echo "Displaying files from handin directory"
+    ls -lA $handinDirectory
 else
     echo "File copying failed. Please verify manually."
 fi
