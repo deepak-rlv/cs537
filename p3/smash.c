9f0f40b4536edc9d0aa907e644e75176106660f2
Deepak Charan L <deepakcharan0108@gmail.com>
2023-02-12 17:31:07 -0600

    Supporting mutiple commands with ;

--- p3/smash.c
+++ p3/smash.c
@@ -21,41 +21,47 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int actionHandler(char * prompt){
-    char **args = malloc(sizeof(char) * (strlen(prompt) + 1));
-
+int stringSplitter(char **multipleCommands, char * prompt, char * delim){
+    char * command = strtok(prompt, delim);
     int i=0;
-    while(1){
-        args[i] = strsep(&prompt," ");
-        if(args[i] == (char *) NULL)
-            break;
+    while (command != NULL){
+        multipleCommands[i] = command;
+        command = strtok(NULL, delim);
         i++;
     }
+    return i;
+}
+
+int actionHandler(char * singleCommand){
+    char **args = malloc(sizeof(char) * (strlen(singleCommand) + 1));
+
+    int numOfArgs = stringSplitter(args, singleCommand, " \n\t") - 1;
 
     if(!strcmp(args[0],"exit")){
         exit(0);
-    } else if(!strcmp(args[0],"cd")){
-        if(args[1] == NULL || args[2] != NULL){
+    } else if(!strcmp(args[0], "cd")){
+        if(numOfArgs > 1 || numOfArgs == 0){
             printf("cd arguments error\n");
+        } else {
+            int chdirReturn = chdir(args[1]);
+            if(chdirReturn < 0){
+                printf("chdir error\n");
+            }
         }
-        int chdirReturn = chdir(args[1]);
-        if(chdirReturn < 0){
-            printf("chdir error\n");
-        }
-    } else if(!strcmp(args[0],"pwd")){
+    } else if(!strcmp(args[0], "pwd")){
         char currentDir[PWD_SIZE];
-        int pwdReturn = getcwd(currentDir, PWD_SIZE);
-        printf("%s\n",currentDir);
-        return 0;
+        if(!getcwd(currentDir, PWD_SIZE));
+        printf("%s\n", currentDir);
     } else {
         int forkReturn = fork();
         if(forkReturn < 0){
             #if debug
                 printf("Fork failed\n");
             #endif
+            free(args);
             return 1;
         } else if(forkReturn == 0){
-            int execReturn = execvp(args[0],args);
+            int execReturn = execvp(args[0], args);
             if (execReturn < 0){
                 #if debug
                     printf("Exec failed.\n");
@@ -69,10 +75,12 @@ int actionHandler(char * prompt){
                 #if debug
                     printf("Wait failed\n");
                 #endif
+                free(args);
                 return 1;
             }
         }
     }
+    free(args);
     return 0;
 }
 
@@ -90,6 +98,7 @@ int main(int argc, char *argv[]){
     int charactersRead;
     while(1){
         printf("smash> ");
+
         charactersRead = getline(&prompt, &promptSize, stdin);
         if(charactersRead == -1){
             #if debug
@@ -103,11 +112,13 @@ int main(int argc, char *argv[]){
             printf("Input Command: %s\n",prompt);
         #endif
 
-        actionHandler(prompt);
+        char **multipleCommands = malloc(sizeof(char) * (strlen(prompt) + 1));
+        int numberOfCommands = stringSplitter(multipleCommands, prompt, ";");
 
+        for(int i = 0; i < numberOfCommands; i++)
+            actionHandler(multipleCommands[i]);
+        
+        free(multipleCommands);
     }
-
-    exit(0);
-
     return 0;
 }
