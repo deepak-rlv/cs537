ced47fe0e76b56d9c176cf9966bca53d859cdc5f
Deepak Charan L <deepakcharan0108@gmail.com>
2023-02-12 16:27:20 -0600

    Added cd & pwd built-in calls

--- p3/smash.c
+++ p3/smash.c
@@ -13,22 +13,65 @@
     #define debug 0
 #endif
 
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
+#define PWD_SIZE 200
 
-int actionHandler(char * path, ...){
-    char * argumentsPtr = (char *)&path + 1;
-    int forkReturn = fork();
-    if(forkReturn < 0){
-        #if debug
-            printf("Fork failed\n");
-        #endif
-        return 1;
-    } else if(forkReturn == 0){
-        execv("/bin",path);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int actionHandler(char * prompt){
+    char **args = malloc(sizeof(char) * (strlen(prompt) + 1));
+
+    int i=0;
+    while(1){
+        args[i] = strsep(&prompt," ");
+        if(args[i] == (char *) NULL)
+            break;
+        i++;
+    }
+
+    if(!strcmp(args[0],"exit")){
+        exit(0);
+    } else if(!strcmp(args[0],"cd")){
+        if(args[1] == NULL || args[2] != NULL){
+            printf("cd arguments error\n");
+        }
+        int chdirReturn = chdir(args[1]);
+        if(chdirReturn < 0){
+            printf("chdir error\n");
+        }
+    } else if(!strcmp(args[0],"pwd")){
+        char currentDir[PWD_SIZE];
+        int pwdReturn = getcwd(currentDir, PWD_SIZE);
+        printf("%s\n",currentDir);
+        return 0;
     } else {
-        int waitReturn = wait(NULL);
+        int forkReturn = fork();
+        if(forkReturn < 0){
+            #if debug
+                printf("Fork failed\n");
+            #endif
+            return 1;
+        } else if(forkReturn == 0){
+            int execReturn = execvp(args[0],args);
+            if (execReturn < 0){
+                #if debug
+                    printf("Exec failed.\n");
+                #endif
+                exit(0);
+            }
+            exit(0);
+        } else {
+            int waitReturn = wait(NULL);
+            if(waitReturn < 0){
+                #if debug
+                    printf("Wait failed\n");
+                #endif
+                return 1;
+            }
+        }
     }
     return 0;
 }
@@ -45,7 +88,7 @@ int main(int argc, char *argv[]){
     char *prompt = malloc(sizeof(char) * promptSize);
     
     int charactersRead;
-    do{
+    while(1){
         printf("smash> ");
         charactersRead = getline(&prompt, &promptSize, stdin);
         if(charactersRead == -1){
@@ -60,17 +103,9 @@ int main(int argc, char *argv[]){
             printf("Input Command: %s\n",prompt);
         #endif
 
-        actionHandler("ls");
+        actionHandler(prompt);
 
-        /* switch(1){
-            case 1   :   actionHandler("ls");
-                            break;
-
-            default     :   //printf("Command not found\n");
-                            break;
-        } */
-
-    }while(strcmp(prompt,"exit"));
+    }
 
     exit(0);
 
