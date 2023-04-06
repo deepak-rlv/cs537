4ecd6d684b2443ffbb1ef4ddfa427dc7cb3fd350
Deepak Charan L <deepakcharan0108@gmail.com>
2023-04-06 18:48:52 -0500

    P6 first commit

---
+++ p6/README.md
@@ -0,0 +1,18 @@
+# **Project 5**
+
+## **Parallel Sort Algorithm**
+
+Submitted by Deepak Charan Logavaseekaran (logavaseekar@wisc.edu)
+<br> <br>
+**CS Login:** ```logavaseekaran```
+<br>
+**Wisc ID:** 908 495 9841
+<br>
+**Status:** All tests passing 
+<br>
+**Section:** Prof. Mike Swift's
+<br>
+**Group:** 9
+<br>
+**Partner's Name:** Vishal Naik
+<br>
---
+++ p6/psort.c
@@ -0,0 +1,27 @@
+/**
+ * @file psort.c
+ * @author Deepak Charan Logavaseekaran (logavaseekar@wisc.edu)
+ * @author Vishal Naik (vvnaik2@wisc.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-06
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+/*
+    Set flag to 1 to view debug information
+    By default, set to 0
+*/
+#ifndef debug
+    #define debug 0
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    return 0;
+}
