0c1771dd4adc2a145121e2cb64c3e935a3c40dca
Deepak Charan L <deepakcharan0108@gmail.com>
2023-04-09 15:40:58 -0500

    Single threaded sorting working-but in input fle

--- p6/psort.c
+++ p6/psort.c
@@ -19,12 +19,89 @@
 #endif
 
 #define ARGUMENTS 3
+#define MIN_THREADS 1
 #define RECORD_SIZE 100
-#define ARRAY_SIZE RECORD_SIZE + 1
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+typedef struct{
+    int key;
+    char *val;
+} stream;
+
+void merge(char *arr, int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = (m - l)/100 + 1;
+    int n2 = (r - m)/100;
+
+    char L[n1][RECORD_SIZE], R[n2][RECORD_SIZE];
+
+    for (i = 0; i < n1; i++)
+        memcpy(L[i], arr + l + i * 100, RECORD_SIZE);
+    for (j = 0; j < n2; j++)
+        memcpy(R[j], arr + m + 100 + j * 100, RECORD_SIZE);
+
+    i = 0;
+    j = 0;
+    k = l / 100;
+    while (i < n1 && j < n2) {
+        int key1 = *(int*)L[i];
+        int key2 = *(int*)R[j];
+        if (key1 <= key2) {
+            memcpy(arr + k * 100, L[i], RECORD_SIZE);
+            i++;
+        }
+        else {
+            memcpy(arr + k * 100, R[j], RECORD_SIZE);
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        memcpy(arr + k * 100, L[i], RECORD_SIZE);
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        memcpy(arr + k * 100, R[j], RECORD_SIZE);
+        j++;
+        k++;
+    }
+}
+ 
+
+void mergeSort(char *inputMMAP, int l, int r)
+{
+    if (l < r) {
+        int m = ((l + (r - l) / 2 ) / 100 ) * 100;
+
+        mergeSort(inputMMAP, l, m);
+        mergeSort(inputMMAP, m + 100, r);
+
+        merge(inputMMAP, l, m, r);
+    }
+}
+
+struct args {
+    char *input;
+    int start, end;
+};
+
+void mergeHelper(void *tArgs){
+    struct args *arg = (struct args*) tArgs;
+    mergeSort(arg->input, arg->start, arg->end);
+}
 
 int main(int argc, char *argv[]){
     if(argc - 1 != ARGUMENTS){
@@ -40,15 +117,15 @@ int main(int argc, char *argv[]){
     /*
         The number of threads for a process cannot be less than 1
     */
-    int threads = atoi(argv[3]);
-    if(threads < 1){
+    int numOfThreads = atoi(argv[3]);
+    if(numOfThreads < MIN_THREADS){
         #ifdef debug
             printf("Number of threads cannot be less than 1\n");
         #endif
         return 0;
     }
     #ifdef debug
-        printf("Number of threads: %d\n", threads);
+        printf("Number of threads: %d\n", numOfThreads);
     #endif
 
     char *inputFile = argv[1];
@@ -57,23 +134,46 @@ int main(int argc, char *argv[]){
         printf("Input File: %s\nOutput File: %s\n", inputFile, outputFile);
     #endif
 
-    FILE *inputFP;
-    if((inputFP = fopen(inputFile, "r")) != 0){
+    int inputFD;
+    if((inputFD = open(inputFile, O_RDWR)) == -1){
         #ifdef debug
-            printf("Input fopen failed\n");
+            printf("Input open failed\n");
         #endif
         return 0;
     }
 
-    FILE *outputFP;
-    if((outputFP = fopen(outputFile, "w")) != 0){
+    int outputFD;
+    if((outputFD = open(outputFile, O_CREAT | O_WRONLY, 0600)) == -1){
         #ifdef debug
-            printf("Output fopen failed\n");
+            printf("Output open failed\n");
         #endif
         return 0;
     }
 
-    // char *records[ARRAY_SIZE] = fgets( char *buf, int n, FILE *fp );
+    struct stat inputFileStats;
+    fstat(inputFD, &inputFileStats);
+    const int entries = inputFileStats.st_size / RECORD_SIZE;
+    #ifdef debug
+        printf("Number of entries in the input file: %d\n", entries);
+    #endif
+
+    struct args tArgs;
+    int mid = entries / numOfThreads;
+
+    char * inputRecords = (char *)mmap(NULL, inputFileStats.st_size, PROT_READ|PROT_WRITE, MAP_SHARED, inputFD, 0);
+    close(inputFD);
+    pthread_t threads[numOfThreads];
+
+    for(int i = 0; i < numOfThreads; i++){
+        tArgs.start = i * mid * RECORD_SIZE;
+        tArgs.end = tArgs.start + (mid - 1) * RECORD_SIZE;
+        tArgs.input = inputRecords;
+        pthread_create(&threads[i], NULL, (void *)mergeHelper, (void *)&tArgs);   
+    }
+
+    for(int i = 0; i < numOfThreads; i++){
+        pthread_join(threads[i], NULL);
+    }
 
     return 0;
 }
