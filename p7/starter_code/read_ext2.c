36748631b9294103de856b8e10ca778c05f90f21
Deepak Logavaseekaran <logavaseekaran@rockhopper-02.cs.wisc.edu>
2023-04-23 20:10:25 -0500

    Updates from CSL

--- p7/starter_code/read_ext2.c
+++ p7/starter_code/read_ext2.c
@@ -16,6 +16,7 @@ unsigned int inodes_per_group = 0;
 
 int debug = 1;          //turn on/off debug prints
 
+
 /* read the first super block to initialize common variables */
 void ext2_read_init(int fd)
 {
@@ -29,7 +30,7 @@ void ext2_read_init(int fd)
         }
 
         block_size = 1024 << super.s_log_block_size;
-		inodes_per_block = block_size / sizeof(struct ext2_inode);		/* number of inodes per block */
+		inodes_per_block = block_size /  super.s_inode_size;		/* number of inodes per block */
 		itable_blocks = super.s_inodes_per_group / inodes_per_block;		/* size in blocks of the inode table */
 		blocks_per_group = super.s_blocks_per_group;
 		num_groups = (super.s_blocks_count + blocks_per_group - 1) / blocks_per_group;
@@ -83,6 +84,21 @@ int powersBelow(int m, int n)
     return cnt;
 }
 
+static inline int has_blockdescriptor_copy(int ngroup) 
+{
+	return ngroup == 0 ||
+		ngroup == 1;
+}
+
+static inline int has_superblock_copy(int ngroup) 
+{
+	return ngroup == 0 ||
+		ngroup == 1 ||
+		isPowerOf(ngroup, 3) ||
+		isPowerOf(ngroup, 5) ||
+		isPowerOf(ngroup, 7) ;
+}
+
 /* read the first super block; for this project, you will only deal with the first block group */
 int read_super_block( int                      fd,        /* the disk image file descriptor */
 					   int                      ngroup,        /* which block group to access */
@@ -90,12 +106,7 @@ int read_super_block( int                      fd,        /* the disk image file
 						)
 {
 	// The groups that have copies of a super block are 0, 1 and powers of 3, 5 and 7
-    if (ngroup != 0 &&
-		ngroup != 1 &&
-		!isPowerOf(ngroup, 3) &&
-		!isPowerOf(ngroup, 5) &&
-		!isPowerOf(ngroup, 7) 
-		)
+    if (!has_superblock_copy(ngroup))
 		{
 			if (debug)
 				printf("this block does not contain a super block copy");
@@ -109,10 +120,15 @@ int read_super_block( int                      fd,        /* the disk image file
 		num_no_super_copy_blocks+=powersBelow(ngroup,5);
 		num_no_super_copy_blocks+=powersBelow(ngroup,7);*/
     
-	
-	    lseek(fd, BASE_OFFSET + BLOCK_OFFSET(blocks_per_group * ngroup), SEEK_SET);        /* position head above super-block */
+		lseek(fd, BLOCK_OFFSET( (blocks_per_group * ngroup + has_superblock_copy(ngroup)) ), SEEK_SET);
+	    //lseek(fd, BASE_OFFSET + BLOCK_OFFSET(blocks_per_group * ngroup), SEEK_SET);        /* position head above super-block */
         read(fd, super, sizeof(struct ext2_super_block));              /* read super-block */
-
+		
+		if (!has_superblock_copy(ngroup)) {
+            if (debug)
+				printf("Block %d does not contain a super block copy\n", ngroup);
+			return -1;
+        }
         if (super->s_magic != EXT2_SUPER_MAGIC) {
                 fprintf(stderr, "read_super_block: Not a Ext2 filesystem\n");
                 exit(1);
@@ -145,30 +161,28 @@ int read_super_block( int                      fd,        /* the disk image file
 		}
 		
 		
-		inodes_per_block = block_size / sizeof(struct ext2_inode);		/* number of inodes per block */
+		inodes_per_block = block_size / super->s_inode_size;		/* number of inodes per block */
 		itable_blocks = super->s_inodes_per_group / inodes_per_block;		/* size in blocks of the inode table */
 		
 		return 0;
 }
 
 /* Read the first group-descriptor in the first block group; you will not be tested with a disk image with more than one block group */
-void read_group_desc( int                      fd,        /* the disk image file descriptor */
+int read_group_desc( int                      fd,        /* the disk image file descriptor */
 					  int                      ngroup,        /* which block group to access */
 		      struct ext2_group_desc *group     /* where to put the group-descriptor */
 					)
 {
-		if (ngroup != 0 &&
-		ngroup != 1 &&
-		isPowerOf(ngroup, 3) &&
-		isPowerOf(ngroup, 5) &&
-		isPowerOf(ngroup, 7) 
-		)
+		printf("Block %d: %d %d\n", ngroup, (blocks_per_group * ngroup + 2), BLOCK_OFFSET((blocks_per_group * ngroup + 2)));
+		if (!has_blockdescriptor_copy(ngroup))
 		{
-			lseek(fd, BASE_OFFSET + BLOCK_OFFSET(blocks_per_group * ngroup), SEEK_SET);
+			if (debug)
+				printf("Block %d does not contain a group descriptor copy\n", ngroup);
+			return -1;
 		}
 		else
 		{
-			lseek(fd, BASE_OFFSET + BLOCK_OFFSET(blocks_per_group * ngroup) + block_size, SEEK_SET);
+			lseek(fd, BLOCK_OFFSET( (blocks_per_group * ngroup + has_blockdescriptor_copy(ngroup) + has_superblock_copy(ngroup)) ), SEEK_SET);
 		}
         read(fd, group, sizeof(struct ext2_group_desc));
 
@@ -195,17 +209,25 @@ void read_group_desc( int                      fd,        /* the disk image file
 off_t locate_inode_table( int ngroup, const struct ext2_group_desc *group      /* the first group-descriptor */
 							    )
 {
-		return BLOCK_OFFSET(group->bg_inode_table + blocks_per_group * ngroup);
+	return BLOCK_OFFSET( (group->bg_inode_table + blocks_per_group * ngroup - !has_superblock_copy(ngroup) - !has_blockdescriptor_copy(ngroup)) );
+	// int no_superblock = (ngroup != 0 && ngroup != 1 && !isPowerOf(ngroup, 3) && !isPowerOf(ngroup, 5) && !isPowerOf(ngroup, 7)) ? 1 : 0;
+	// 	int no_group_desc = ngroup > 1 ? 1 : 0;
+	// 	return BLOCK_OFFSET( (group->bg_inode_table - no_superblock - no_group_desc + blocks_per_group * ngroup) );
 }
 
 /* calculate the start address of the data blocks in the first group */
 off_t locate_data_blocks( int ngroup, const struct ext2_group_desc *group      /* the first group-descriptor */
 							    )
 {
-		return BLOCK_OFFSET(group->bg_inode_table + itable_blocks + blocks_per_group * ngroup);
+		return BLOCK_OFFSET( group->bg_inode_table + itable_blocks + blocks_per_group * ngroup );
 }
 
-void read_inode(int fd, off_t offset, int inode_no, struct ext2_inode *inode, __u16 s_inode_size)
+void read_inode(fd, offset, inode_no, inode, s_inode_size)
+     int                            fd;        /* the floppy disk file descriptor */
+     off_t 			   				offset;    /* offset to the start of the inode table */
+     int                            inode_no;  /* the inode number to read  */
+     struct ext2_inode             *inode;     /* where to put the inode */
+	 __u16							s_inode_size;
 {
         lseek(fd, offset + (inode_no-1)*s_inode_size, SEEK_SET);
         read(fd, inode, sizeof(struct ext2_inode));
